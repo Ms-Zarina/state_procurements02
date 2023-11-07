c28b2bd4f10809d66cd0708c1c8e368a7157778f
Ms-Zarina <zarina.maratova02@gmail.com>
2023-11-08 00:35:40 +0600

    comit2

--- src/pages/AnnouncPage/AnnouncPage.jsx
+++ src/pages/AnnouncPage/AnnouncPage.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from 'react';
-import Public from '../../components/public1/Public'
+// import { useEffect } from 'react';
+import Public from '../../components/public1/Public';
 
 function AnnouncPage() {
-
-    useEffect(() => {
-        fetch('http://159.223.108.242/api/tender/')
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-            })
-    })
+  // useEffect(() => {
+  //     fetch('http://159.223.108.242/api/tender/')
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //             console.log(data)
+  //         })
+  // })
   return (
     <div>
-      <Public/>
+      <Public />
+      паблик
     </div>
   );
 }
